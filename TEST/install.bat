df59a9bd4afdeb7f42dcd4b35e84b4dad5b3f0e5
senselogic <dev.senselogic@gmail.com>
2024-04-14 20:40:54 +0200

    % scripts

--- TEST/install.bat
+++ TEST/install.bat
@@ -1,5 +1,5 @@
 python.exe --version
 echo requires python 3.11.7+
-python.exe -m pip install --upgrade pip"
+python.exe -m pip install --upgrade pip
 pip install pandas sacremoses sentencepiece torch transformers
 pause
